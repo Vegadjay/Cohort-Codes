f2154822ca09d74fd9df56b8f4beae14e6988c16
Vegadjay <jayvegad10@gmail.com>
2025-03-13 01:08:26 +0530

    Complementry Color Simple Project

---
+++ complexcolorpicker/README.md
@@ -0,0 +1,13 @@
+# Complemetry Color Using Html and Javascript
+
+## Features
+
+- Real-time color chnage.
+- Showing Color to side by side.
+- Add all formate colors as per your choice.
+
+## How It Works
+
+- Store that color hexa code. Parse that code. XOR with 0XFFFFFF. And than complemenry color is find. Assign value to all the divisions.
+
+![Complemenry Color Code Screenshot](./image.png)
--- complexcolorpicker/index.js
+++ complexcolorpicker/index.js
@@ -1,6 +1,9 @@
 const originalColor = document.getElementById("orignal-color");
 const colorPicker = document.getElementById("color-picker");
 const complementaryColors = document.getElementById("complementary-color");
+const colorOfText = document.getElementById("complementary-color-text");
+const originalColorText = document.getElementById("orignal-color-text");
+const littleBox = document.getElementById("complementry-color-little-div");
 
 colorPicker.addEventListener("input", () => {
     const inputColor = colorPicker.value;
@@ -9,9 +12,22 @@ colorPicker.addEventListener("input", () => {
     const complementaryColor = 0xFFFFFF ^ newColor;
     const fnComplementary = "#" + complementaryColor.toString(16).padStart(6, "0");
 
+    littleBox.style.backgroundColor = fnComplementary;
     originalColor.style.backgroundColor = inputColor;
     complementaryColors.style.backgroundColor = fnComplementary;
 
-    console.log("Original Color:", inputColor);
-    console.log("Complementary Color:", fnComplementary);
+    colorOfText.style.color = inputColor;
+    originalColorText.style.color = fnComplementary;
+
+    littleBox.dataset.color = fnComplementary;
+});
+
+littleBox.addEventListener("click", () => {
+    const colorToCopy = littleBox.dataset.color;
+
+    navigator.clipboard.writeText(colorToCopy).then(() => {
+        alert(`Copied: ${colorToCopy}`);
+    }).catch(err => {
+        console.error("Failed to copy: ", err);
+    });
 });
